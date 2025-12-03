a9ce64b186009d351075d90c989da6e25d7259d2
Stefan <stefanprati@gmail.com>
2025-12-03 16:30:58 -0300

    🔥 Hotfix: Resolve travamento no loading do CRM
    - Move firebaseConfig para inline no firebase.js (solução temporária)
    - Remove import problemático que causava SyntaxError
    - Adiciona autocomplete='username' ao campo createEmail
    - Resolve travamento 'Carregando CRM...'
    
    BREAKING: firebaseConfig agora é inline (temporário)
    FIX: Aplicação não trava mais no loading
    FIX: Remove warnings de autocomplete
    HOTFIX: Crítico - aplicação voltou a funcionar

--- public/app/core/firebase.js
+++ public/app/core/firebase.js
@@ -15,7 +15,18 @@ import {
   getDoc
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
-import { firebaseConfig } from "../config/firebaseConfig.js";
+// Configuração do Firebase (inline temporário)
+// TODO: Mover para ../config/firebaseConfig.js com export correto
+const firebaseConfig = {
+  apiKey: "AIzaSyBD_qBqWHHnq1QQjROI2jkJu1K6RbBnE",
+  authDomain: "crm-energia-solar.firebaseapp.com",
+  projectId: "crm-energia-solar",
+  storageBucket: "crm-energia-solar.firebasestorage.app",
+  messagingSenderId: "83187644189",
+  appId: "1:83187644189:web:d3cf98a894e87c2c8093f4",
+  measurementId: "G-QC5dRD90W"
+};
+
 import { CRMApp } from "./crmApp.js";
 import { showToast } from "../ui/toast.js";
 
