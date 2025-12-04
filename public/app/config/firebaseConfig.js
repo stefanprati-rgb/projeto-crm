efb332eb9f23abfd0a2cae18b1a64e5bacec1cd9
Stefan <stefanprati@gmail.com>
2025-12-04 10:56:33 -0300

    Feat: Implementação de Tickets, Timeline, Tarefas e correção de API Key

--- public/app/config/firebaseConfig.js
+++ public/app/config/firebaseConfig.js
@@ -1,10 +1,9 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
-  apiKey: "AIzaSyBD_qBqWHHnq1QQjROI2jkJu1K6RbBnE",  // ✅ CORRIGIDO
+  apiKey: "AIzaSyBO_jqBqWHHnq1QQlROi2lajKu1K6RbBnE",
   authDomain: "crm-energia-solar.firebaseapp.com",
   projectId: "crm-energia-solar",
   storageBucket: "crm-energia-solar.firebasestorage.app",
   messagingSenderId: "83187644189",
   appId: "1:83187644189:web:d3cf98a894e87c2c8093f4",
-  measurementId: "G-QC5dRD90W"
+  measurementId: "G-QCE6RD0QDW"
 };
