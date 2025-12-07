19e7b5283a45fff859a658173b7da7cd16ce0ae8
Stefan <stefanprati@gmail.com>
2025-12-07 11:07:54 -0300

    chore: Atualizar configuração do Firebase (measurementId)

--- public/app/config/firebaseConfig.js
+++ public/app/config/firebaseConfig.js
@@ -22,14 +22,15 @@ function validateConfig(config) {
   return true;
 }
 
-export const firebaseConfig = {
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
   apiKey: "AIzaSyBO_jqBqWHHnq1QQlROi2lajKu1K6RbBnE",
   authDomain: "crm-energia-solar.firebaseapp.com",
   projectId: "crm-energia-solar",
   storageBucket: "crm-energia-solar.firebasestorage.app",
   messagingSenderId: "83187644189",
   appId: "1:83187644189:web:d3cf98a894e87c2c8093f4",
-  measurementId: "G-QC5dRD90W"
+  measurementId: "G-QCE6RD0QDW"
 };
 
 // Validar configuração ao carregar
